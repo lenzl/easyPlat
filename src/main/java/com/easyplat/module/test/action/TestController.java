c66aa7b7b64709dc29ba72847ed84f04ebe10ea5
lenzl <29360874@qq.com>
2016-03-28 16:25:29 +0800

    增加@InitBinder

--- src/main/java/com/easyplat/module/test/action/TestController.java
+++ src/main/java/com/easyplat/module/test/action/TestController.java
@@ -3,18 +3,23 @@ package com.easyplat.module.test.action;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.easyplat.module.test.domain.TestUser;
 import com.easyplat.module.test.service.TestService;
 
 @Controller
-public class TestController {
+public class TestController extends TestBaseController{
 	
 	@Autowired
 	private TestService testService;
 	
 	@RequestMapping(value="/simple")
-	public @ResponseBody String simple() {
+	public @ResponseBody String simple(TestUser user) {
+		
+		System.out.println(user.getName());
+		System.out.println(user.getBirthday());
 		
 		testService.print();
 		
